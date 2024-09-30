ad17fa408aaf3196c1ba271257666100ab323204
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-09-30 14:54:30 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen --git-lfs --depth=1
 #Local manifests
 git clone https://github.com/FreakyFriday12/local_manifests -b udc-2 .repo/local_manifests
 
