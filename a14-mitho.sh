53f0f5d83dd44c966b88380ea694b8570887e617
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-09-30 16:09:02 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,14 +1,15 @@
-rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
-git clone https://github.com/FreakyFriday12/Local-Manifest --depth 1 -b Voltage-14 .repo/local_manifests  
-# Sync the repositories
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-# Set up build environment
-export EVO_BUILD_TYPE=Unofficial
-echo 'export EVO_BUILD_TYPE=Unofficial' >> ~/.bashrc
-source ~/.bashrc
-. build/envsetup.sh
- 
-# Build the ROM
-lunch lineage_earth-userdebug
-m evolution
+rm -rf .repo/local_manifests/
+#repo init rom
+repo init -u romsource -b branch --git-lfs
+#Local manifests
+git clone yourmanifest -b branch .repo/local_manifests &&
+
+#build
+/opt/crave/resync.sh
+source build/envsetup.sh
+export BUILD_USERNAME=Ditya 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
+export TZ=Asia/Makassar
+lunch lineage_earth-userdebug #edit this according to your rom build commands
+make bacon #also edit this if needed
