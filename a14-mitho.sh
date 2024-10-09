be5f5983c2cae3da707744608a309302d413636b
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-10-09 21:23:01 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,9 +1,11 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
+
 #Local manifests
 git clone https://raw.githubusercontent.com/FreakyFriday12/Local-Manifest/refs/heads/Voltage-14/Voltage-14.xml -b Voltage-14 .repo/local_manifests &&
-
+# Sync 
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 #build
 /opt/crave/resync.sh
 source build/envsetup.sh
@@ -11,5 +13,5 @@ export BUILD_USERNAME=Ditya
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export TZ=Asia/Makassar
-lunch derp_earth-userdebug #edit this according to your rom build commands
-mka derp #also edit this if needed
+lunch cipher_earth-userdebug #edit this according to your rom build commands
+mka cipher #also edit this if needed
