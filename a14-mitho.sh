2d9a283437eaf65b721cb72ab8de8e9cc6f44960
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-10-11 15:08:07 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,10 +1,10 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init --depth=1 -u https://github.com/Project-Elixir/manifest -b UNO --git-lfs
+repo init -u https://github.com/xdroid-oss/xd_manifest -b fourteen --git-lfs
 #local manifest
 git clone https://raw.githubusercontent.com/FreakyFriday12/Local-Manifest/refs/heads/Voltage-14/Voltage-14.xml -b Voltage-14 .repo/local_manifests &&
 # Sync 
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 #build
 /opt/crave/resync.sh
 source build/envsetup.sh
@@ -16,5 +16,5 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export TZ=Asia/Makassar
 # Choose a target device 
 source build/envsetup.sh
-lunch aosp_earth-userdebug
-mka bacon -jX
+lunch xdroid_earth-userdebug
+make xd -j$(nproc --all)
