124cbd22842d11c3b7475fbbf4ef0021865b5e33
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-10-12 05:30:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/xdroid-oss/xd_manifest -b fourteen --git-lfs
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
 #local manifest
 git clone https://raw.githubusercontent.com/FreakyFriday12/Local-Manifest/refs/heads/Voltage-14/Voltage-14.xml -b Voltage-14 .repo/local_manifests &&
 # Sync 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync
 #build
 /opt/crave/resync.sh
 source build/envsetup.sh
@@ -14,5 +14,5 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export TZ=Asia/Makassar
 # Choose a target device 
 . build/envsetup.sh
-lunch xdroid_earth-ap1a-userdebug
-make xd -j$(nproc --all)
+lunch aosp_earth-ap2a-userdebug
+mka bacon
