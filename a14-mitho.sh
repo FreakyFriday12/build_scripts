0e81cf42ba8ef0ef1731143567040fd4052311d9
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-10-11 14:58:18 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/yaap/manifest.git -b fourteen --git-lfs
+repo init --depth=1 -u https://github.com/Project-Elixir/manifest -b UNO --git-lfs
 #local manifest
 git clone https://raw.githubusercontent.com/FreakyFriday12/Local-Manifest/refs/heads/Voltage-14/Voltage-14.xml -b Voltage-14 .repo/local_manifests &&
 # Sync 
@@ -10,8 +10,11 @@ repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
 source build/envsetup.sh
 export BUILD_USERNAME=Ditya 
 export BUILD_HOSTNAME=crave
+export PROJECTFOLDER="/crave-devspaces/Lineage21"
+export PROJECTID="72"
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export TZ=Asia/Makassar
 # Choose a target device 
 source build/envsetup.sh
-lunch yaap_earth-userdebug && m yaap
+lunch aosp_earth-userdebug
+mka bacon -jX
